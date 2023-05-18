04a7fa79e6a8cba377469da7ec65b6e88cced248
yinkakun <yinkakun@gmail.com>
2023-05-18 11:57:00 +0200

    feat(components): add FadeIn component for animations.

---
+++ src/app/components/fade-in.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { motion } from "framer-motion";
+
+interface FadeInProps {
+  children: React.ReactNode;
+}
+
+export const FadeIn = ({ children }: FadeInProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeIn" }}
+    >
+      {children}
+    </motion.div>
+  );
+};
