f20df344b474dcdfbd87998273a2d6710dee5146
yinkakun <yinkakun@gmail.com>
2023-05-18 11:56:49 +0200

    refactor(tailwind-config): simplify css variables and add gradient animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,56 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx}",
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-		// Or if using `src` directory:
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		// colors: {
-		// 	"button-green": "#02FEF1",
-		// 	"darker-green": "#035554",
-		// 	"button-purple": "#AA01FC",
-		// },
-		// backgroundImage: {
-		// 	background:
-		// 		"url('/Users/opeyemibabalola/Desktop/portfolio/my-portfolio/assets/image.png')",
-		// },
-		extend: {},
-	},
-	plugins: [],
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "gradient-cyan-purple":
+          "linear-gradient(90deg, #02FEF1 0%, #AA01FC 100%)",
+      }),
+    },
+    fontFamily: {
+      mono: ["var(--font-inconsolata)", ...fontFamily.mono],
+    },
+    keyframes: {
+      "gradient-y": {
+        "0%, 100%": {
+          "background-size": "200% 200%",
+          "background-position": "center top",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "center center",
+        },
+      },
+      "gradient-x": {
+        "0%, 100%": {
+          "background-size": "200% 200%",
+          "background-position": "left center",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "right center",
+        },
+      },
+      "gradient-xy": {
+        "0%, 100%": {
+          "background-size": "400% 400%",
+          "background-position": "left center",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "right center",
+        },
+      },
+    },
+    animation: {
+      "spin-slow": "spin 10s linear infinite",
+      "gradient-x": "gradient-x 5s ease infinite",
+      "gradient-y": "gradient-y 5s ease infinite",
+      "gradient-xy": "gradient-xy 5s ease infinite",
+    },
+  },
+  plugins: [],
 };
